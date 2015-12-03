13c0d14daedd52f1ee444f1c3d754fed4a7eb70b
Jen Kahn <jenniferhkahn@gmail.com>
2015-12-03 12:15:41 -0600

    initial push

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+
+gem 'sinatra'
+gem 'pg'
+gem 'pry'
+gem 'sinatra-activerecord'
+gem 'json'
+gem 'bcrypt' # our security library
---
+++ db/migrations.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE bee_crypt_bzz;
+\c bee_crypt_bzz
+
+CREATE TABLE accounts (id SERIAL PRIMARY KEY, user_nane VARCHAR(255), user_email (VARCHAR(255), password_digest VARCHAR(255))
