f91d0bf357a7ca07e3c5b52c74c1fc3237b0806a
Jen Kahn <jenniferhkahn@gmail.com>
2015-12-03 12:23:31 -0600

    working with db file

--- app.rb
+++ app.rb
@@ -1,2 +1,17 @@
 require 'bundler'
 Bundler.require
+
+#establishing connection to postgresql db
+
+ActiveRecord::Base.establish_connection(
+  :database => 'bee_crypt_bzz',
+  :adapter => 'postgresql'
+
+)
+
+
+#basic template routes
+get '/' do
+return {:hello => 'world'}.to_json
+
+end
--- config.ru
+++ config.ru
@@ -1,2 +1,4 @@
 require './app'
+require './models/account' #models/account.rb
+
 run Sinatra::Application
